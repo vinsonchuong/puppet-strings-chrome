c4be5995508bc7a12605e846ec2e70907e127927
Vinson Chuong <vinsonchuong@gmail.com>
2019-05-24 21:58:18 -0700

    chore(travis): Try finding Chrome on Windows

--- actions/find-chrome/index.js
+++ actions/find-chrome/index.js
@@ -4,19 +4,39 @@ import * as path from 'path'
 import fs, { promises as fsp } from 'fs'
 
 export default async function(): Promise<?string> {
+  const possiblePaths = []
+
   if (process.platform === 'darwin') {
-    const possiblePaths = [
+    possiblePaths.push(
       '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-      path.join(os.homedir(), '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome')
-    ]
-
-    for (const possiblePath of possiblePaths) {
-      try {
-        await fsp.access(possiblePath, fs.constants.R_OK | fs.constants.X_OK)
-        return possiblePath
-      } catch (error) {}
-    }
-
-    return null
+      path.join(
+        os.homedir(),
+        '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+      )
+    )
   }
+
+  if (process.platform === 'linux') {
+    possiblePaths.push(
+      '/usr/bin/chromium',
+      '/usr/bin/chromium-browser',
+      '/usr/bin/google-chrome',
+      '/usr/bin/google-chrome-stable'
+    )
+  }
+
+  if (process.platform === 'win32') {
+    possiblePaths.push(
+      'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
+    )
+  }
+
+  for (const possiblePath of possiblePaths) {
+    try {
+      await fsp.access(possiblePath, fs.constants.R_OK | fs.constants.X_OK)
+      return possiblePath
+    } catch (error) {}
+  }
+
+  return null
 }
--- actions/find-chrome/index.test.js
+++ actions/find-chrome/index.test.js
@@ -4,8 +4,12 @@ import findChrome from './'
 
 test('finding the path to a locally installed version of Chrome', async t => {
   if (process.platform === 'darwin') {
-    t.true(await findChrome(), '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome')
+    t.is(await findChrome(), '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome')
+  } else if (process.platform === 'linux') {
+    t.is(await findChrome(), '/usr/bin/chromium')
+  } else if (process.platform === 'win32') {
+    t.is(await findChrome(), 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe')
+  } else {
+    t.fail()
   }
-
-  t.pass()
 })
